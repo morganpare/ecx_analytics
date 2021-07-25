23c82c39365673b8aef8d8111ae6c107fff21adc
morganpare <morganpare@gmail.com>
2021-07-25 11:08:29 +0100

    Update feature store to include complete data (not just those dates with prices)

--- ecx_analytics/data_processor/silver_to_feature_store.py
+++ ecx_analytics/data_processor/silver_to_feature_store.py
@@ -34,58 +34,61 @@ class SilverToFeatureStore:
         return df[['trade_date','mid_price']].copy()
 
     @staticmethod
-    def calculate_last_price_columns(prices_df):
+    def create_ts_df(start, end):
+        ts_index = pd.date_range(start=start, end=end, freq='1D')
+        return pd.DataFrame({'trade_date':ts_index})
+
+
+    @staticmethod
+    def calculate_last_price_columns(prices_df, ts_df):
         prices_df = prices_df.copy()
+        ts_df = ts_df.copy()
         prices_df['joined_date'] = prices_df['trade_date']
-        df_ = pd.merge_asof(prices_df, prices_df, on='trade_date', suffixes=('','_lagged'), allow_exact_matches=False)
-        df_['last_price_distance'] = (df_['trade_date'] - df_['joined_date_lagged']).dt.days
+        df_ = pd.merge_asof(ts_df, prices_df, on='trade_date', suffixes=('','_lagged'), allow_exact_matches=False)
+        df_['last_price_distance'] = (df_['trade_date'] - df_['joined_date']).dt.days
 
         df_.fillna(method='bfill', inplace=True)
-        df__ = pd.merge_asof(df_, prices_df, left_on='joined_date_lagged', right_on='trade_date', suffixes=('','_twice'), allow_exact_matches=False)
+        df__ = pd.merge_asof(df_, prices_df, left_on='joined_date', right_on='trade_date', suffixes=('','_twice'), allow_exact_matches=False)
         df__['second_last_price_distance'] = (df__['trade_date'] - df__['joined_date_twice']).dt.days
 
-
         return pd.DataFrame({
-            'lag_price':df_['mid_price_lagged'].copy(),
+            'lag_price':df_['mid_price'].copy(),
             'lag_distance':df_['last_price_distance'].copy(),
             'lag_price_twice':df__['mid_price_twice'].copy(),
             'second_last_price_distance':df__['second_last_price_distance'].copy()
         }).reset_index(drop=True)
 
     @staticmethod
-    def calculate_moving_averages(prices_df):
+    def calculate_moving_averages(prices_df, ts_df):
         prices_df = prices_df.copy()
-        start = prices_df['trade_date'].min()
-        end = prices_df['trade_date'].max()
-        time_index = pd.date_range(start=start, end=end, freq='1D')
-        full_df = pd.DataFrame({'full_ts':time_index})
-        full_df = pd.merge(full_df, prices_df, how='left', left_on='full_ts', right_on='trade_date')
-        full_df.set_index(['full_ts'], inplace=True)
-        full_df.drop(columns=['trade_date'], inplace=True)
-        ma_30 = full_df.rolling(window=30, min_periods=0).mean()
-        std_30 = full_df.rolling(window=30, min_periods=0).std()
-        count_30 = full_df.rolling(window=30, min_periods=0).count()
-        ema_01 = full_df.ewm(alpha=0.1, adjust=False, min_periods=0).mean()
-        ema_03 = full_df.ewm(alpha=0.3, adjust=False, min_periods=0).mean()
-        estd_01 = full_df.ewm(alpha=0.1, adjust=False, min_periods=0).std()
-        estd_03 = full_df.ewm(alpha=0.3, adjust=False, min_periods=0).std()
+        ts_df = ts_df.copy()
+        full_df = pd.merge(ts_df, prices_df, how='left', on='trade_date')
+        full_df.set_index(['trade_date'], inplace=True)
+        # full_df.drop(columns=['trade_date'], inplace=True)
+        ma_30 = full_df.rolling(window=30, min_periods=0, closed='left').mean()
+        std_30 = full_df.rolling(window=30, min_periods=0, closed='left').std().fillna(value=0)
+        count_30 = full_df.rolling(window=30, min_periods=0, closed='left').count()
+        ema_01 = full_df.ewm(alpha=0.1, adjust=False, min_periods=0).mean().shift()
+        ema_03 = full_df.ewm(alpha=0.3, adjust=False, min_periods=0).mean().shift()
+        estd_01 = full_df.ewm(alpha=0.1, adjust=False, min_periods=0).std().shift()
+        estd_03 = full_df.ewm(alpha=0.3, adjust=False, min_periods=0).std().shift()
 
-        ma_30 = pd.merge(prices_df, ma_30, left_on='trade_date', right_index=True, suffixes=('','_ma_30'))[['mid_price_ma_30']]
-        std_30 = pd.merge(prices_df, std_30, left_on='trade_date', right_index=True, suffixes=('','_std_30'))[['mid_price_std_30']]
-        count_30 = pd.merge(prices_df, count_30, left_on='trade_date', right_index=True, suffixes=('','_count_30'))[['mid_price_count_30']]
-        ema_01 = pd.merge(prices_df, ema_01, left_on='trade_date', right_index=True, suffixes=('','_ema_01'))[['mid_price_ema_01']]
-        ema_03 = pd.merge(prices_df, ema_03, left_on='trade_date', right_index=True, suffixes=('','_ema_03'))[['mid_price_ema_03']]
-        estd_01 = pd.merge(prices_df, estd_01, left_on='trade_date', right_index=True, suffixes=('','_estd_01'))[['mid_price_estd_01']]
-        estd_03 = pd.merge(prices_df, estd_03, left_on='trade_date', right_index=True, suffixes=('','_estd_03'))[['mid_price_estd_03']]
+        # ma_30 = pd.merge(prices_df, ma_30, left_on='trade_date', right_index=True, suffixes=('','_ma_30'))[['mid_price_ma_30']]
+        # std_30 = pd.merge(prices_df, std_30, left_on='trade_date', right_index=True, suffixes=('','_std_30'))[['mid_price_std_30']]
+        # count_30 = pd.merge(prices_df, count_30, left_on='trade_date', right_index=True, suffixes=('','_count_30'))[['mid_price_count_30']]
+        # ema_01 = pd.merge(prices_df, ema_01, left_on='trade_date', right_index=True, suffixes=('','_ema_01'))[['mid_price_ema_01']]
+        # ema_03 = pd.merge(prices_df, ema_03, left_on='trade_date', right_index=True, suffixes=('','_ema_03'))[['mid_price_ema_03']]
+        # estd_01 = pd.merge(prices_df, estd_01, left_on='trade_date', right_index=True, suffixes=('','_estd_01'))[['mid_price_estd_01']]
+        # estd_03 = pd.merge(prices_df, estd_03, left_on='trade_date', right_index=True, suffixes=('','_estd_03'))[['mid_price_estd_03']]
 
         return pd.concat([
-            ma_30, 
-            std_30,
-            count_30,
-            ema_01,
-            ema_03,
-            estd_01,
-            estd_03,
+            ma_30.rename(columns={'mid_price':'mid_price_ma_30'})[['mid_price_ma_30']],
+            std_30.rename(columns={'mid_price':'mid_price_std_30'})[['mid_price_std_30']],
+            count_30.rename(columns={'mid_price':'mid_price_count_30'})[['mid_price_count_30']],
+            ema_01.rename(columns={'mid_price':'mid_price_ema_01'})[['mid_price_ema_01']],
+            ema_03.rename(columns={'mid_price':'mid_price_ema_03'})[['mid_price_ema_03']],
+            estd_01.rename(columns={'mid_price':'mid_price_estd_01'})[['mid_price_estd_01']],
+            estd_03.rename(columns={'mid_price':'mid_price_estd_03'})[['mid_price_estd_03']],
         ], axis=1).reset_index(drop=True)
 
         # df_ = pd.DataFrame({
@@ -101,45 +104,40 @@ class SilverToFeatureStore:
         # return df_
 
     @staticmethod
-    def calculate_spread_metrics(df):
+    def calculate_spread_metrics(df, ts_df):
         df_ = df[['trade_date','spread']].copy()
-        start = df_['trade_date'].min()
-        end = df_['trade_date'].max()
-        time_index = pd.date_range(start=start, end=end, freq='1D')
-        full_df = pd.DataFrame({'full_ts':time_index})
-        full_df = pd.merge(full_df, df_, how='left', left_on='full_ts', right_on='trade_date')
-        full_df.set_index(['full_ts'], inplace=True)
-        full_df.drop(columns=['trade_date'], inplace=True)
+        ts_df = ts_df.copy()
+        full_df = pd.merge(ts_df, df_, how='left', on='trade_date')
+        full_df.set_index(['trade_date'], inplace=True)
 
-        mean_df = full_df.rolling(window=30, min_periods=0).mean()
-        spread_df = pd.merge(df_, mean_df, left_on='trade_date', right_index=True, suffixes=('','_mean'))[['spread_mean']]
+        mean_df = full_df.rolling(window=30, min_periods=0, closed='left').mean().rename(columns={'spread':'spread_mean'})[['spread_mean']]
+        # spread_df = pd.merge(df_, mean_df, left_on='trade_date', right_index=True, suffixes=('','_mean'))[['spread_mean']]
 
-        return spread_df.reset_index(drop=True)
+        return mean_df.reset_index(drop=True)
 
     @staticmethod
-    def calculate_volume_metrics(df):
+    def calculate_volume_metrics(df, ts_df):
         df_ = df[['trade_date','volume_ton']].copy()
-        start = df_['trade_date'].min()
-        end = df_['trade_date'].max()
-        time_index = pd.date_range(start=start, end=end, freq='1D')
-        full_df = pd.DataFrame({'full_ts':time_index})
-        full_df = pd.merge(full_df, df_, how='left', left_on='full_ts', right_on='trade_date')
-        full_df.set_index(['full_ts'], inplace=True)
-        full_df.drop(columns=['trade_date'], inplace=True)
+        ts_df = ts_df.copy()
+        full_df = pd.merge(ts_df, df_, how='left', on='trade_date')
+        full_df.set_index(['trade_date'], inplace=True)
 
-        mean_df = full_df.rolling(window=30, min_periods=0).mean()
-        total_df = full_df.rolling(window=30, min_periods=0).sum()
+        mean_df = full_df.rolling(window=30, min_periods=0, closed='left').mean()
+        total_df = full_df.rolling(window=30, min_periods=0, closed='left').sum()
+
+        mean_df.rename(columns={'volume_ton':'volume_ton_mean'}, inplace=True)
+        total_df.rename(columns={'volume_ton':'volume_ton_sum'}, inplace=True)
         
-        mean_df = pd.merge(df_, mean_df, left_on='trade_date', right_index=True, suffixes=('','_mean'))[['volume_ton_mean']]
-        total_df = pd.merge(df_, total_df, left_on='trade_date', right_index=True, suffixes=('','_sum'))[['volume_ton_sum']]
+        # mean_df = pd.merge(df_, mean_df, left_on='trade_date', right_index=True, suffixes=('','_mean'))[['volume_ton_mean']]
+        # total_df = pd.merge(df_, total_df, left_on='trade_date', right_index=True, suffixes=('','_sum'))[['volume_ton_sum']]
 
-        return pd.concat([mean_df, total_df], axis=1).reset_index(drop=True)
+        return pd.concat([mean_df[['volume_ton_mean']], total_df[['volume_ton_sum']]], axis=1).reset_index(drop=True)
 
     @staticmethod
-    def get_timestamp_features(df):
-        weekday_number = df['trade_date'].dt.dayofweek
-        month = df['trade_date'].dt.month
-        day_of_month = df['trade_date'].dt.day
+    def get_timestamp_features(ts_df):
+        weekday_number = ts_df['trade_date'].dt.dayofweek
+        month = ts_df['trade_date'].dt.month
+        day_of_month = ts_df['trade_date'].dt.day
 
         weekday_sin = np.sin(2 * np.pi * weekday_number/6.0)
         weekday_cos = np.cos(2 * np.pi * weekday_number/6.0)
@@ -163,22 +161,32 @@ class SilverToFeatureStore:
 
     def append_features(self, df):
         df = df.copy()
+        start = df['trade_date'].min()
+        end = df['trade_date'].max()
+        ts_df = self.create_ts_df(start, end)
         prices_df = self.get_prices_single(df)
-        lag_df = self.calculate_last_price_columns(prices_df)
-        ma_df = self.calculate_moving_averages(prices_df)
-        spread_df = self.calculate_spread_metrics(df)
-        vol_df = self.calculate_volume_metrics(df)
-        ts_df = self.get_timestamp_features(df)
-        print(df)
-        print(lag_df)
-        print(ma_df)
-        print(spread_df)
-        print(vol_df)
-        print(ts_df)
-        return pd.concat([df.reset_index(drop=True), lag_df, ma_df, spread_df, vol_df, ts_df], axis=1)
+        lag_df = self.calculate_last_price_columns(prices_df, ts_df)
+        ma_df = self.calculate_moving_averages(prices_df, ts_df)
+        spread_df = self.calculate_spread_metrics(df, ts_df)
+        vol_df = self.calculate_volume_metrics(df, ts_df)
+        t_df = self.get_timestamp_features(ts_df)
 
-    def store_as_pickle(self, df, symbol):
-        file_path = pathlib.Path.joinpath(self.feature_store_path, f"{symbol}_features.pkl")
+        # concatenate calculated features
+        df_ = pd.concat([ts_df, lag_df, ma_df, spread_df, vol_df, t_df], axis=1)
+
+        # add back in target
+        out_df = pd.merge(df[['trade_date','mid_price']], df_, on='trade_date', how='right')
+        # out_df[['symbol', 'warehouse', 'production_year', 'warehouse_name']] = out_df[['symbol', 'warehouse', 'production_year', 'warehouse_name']].bfill()
+        # remove
+        return out_df
+
+
+    def store_targets_as_pickle(self, df, symbol):
+        file_path = pathlib.Path.joinpath(self.feature_store_path, f"{symbol}_with_target_features.pkl")
+        df.to_pickle(file_path)
+
+    def store_complete_as_pickle(self, df, symbol):
+        file_path = pathlib.Path.joinpath(self.feature_store_path, f"{symbol}_complete_features.pkl")
         df.to_pickle(file_path)
 
     def run(self):
@@ -188,5 +196,7 @@ class SilverToFeatureStore:
             df_ = self.filter_by_symbol(df, s)
             df_ = self.append_features(df_)
             df_ = self.filter_by_date_range(df_, start='2012-03-01')
-            self.store_as_pickle(df_, s)
+            self.store_complete_as_pickle(df_, s)
+            df_with_targets = df_[df_[['mid_price']].notnull().any(axis=1)]
+            self.store_targets_as_pickle(df_with_targets, s)
         return True
